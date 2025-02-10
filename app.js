e98f6111d9e2911925f3a27273f409b0ef0a49aa
prabhat69392 <prabhatkumar909758@gmail.com>
2025-02-10 10:28:22 +0530

    new

--- app.js
+++ app.js
@@ -1,107 +1,111 @@
-const mongoose = require("mongoose");
-const Listing = require("./models/listing.js");
-const express=require("express"); 
-const app= express();
-const path= require("path")
-const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
-const methodOverride= require("method-override")
-const ejsmate= require("ejs-mate");
-const WrapAsync= require("./utils/WrapAsync.js")
-//  const ExpressError= require("./utils/ExpressError.js")
-app.engine("ejs",ejsmate)
-app.set("view engine","ejs") 
-app.set("views",path.join(__dirname,"views"))  
-const bodyParser = require("body-parser");
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(methodOverride("_method"));
-app.use(express.static (path.join(__dirname,"/public")))
-main()
-  .then(() => {
-    console.log("connected to DB");
-  })
-  .catch((err) => {
-    console.log(err) ;
-  });
- let port=3000;
- app.listen(port, (req,res) => {
-   console.log("port is listing") 
- })
+  const mongoose = require("mongoose");
+  const Listing = require("./models/listing.js");
+  const express=require("express"); 
+  const app= express();
+  const path= require("path")
+  const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
+  const methodOverride= require("method-override")
+  const ejsmate= require("ejs-mate");
+  const WrapAsync= require("./utils/WrapAsync.js")
+  const ExpressError= require("./utils/ExpressError.js")
+  const {listingSchema}= require("./schema.js")
 
-async function main() {
-  await mongoose.connect(MONGO_URL);
-}
-  
-// const initDB = async () => {
-//   await Listing.deleteMany({});
-//   await Listing.insertMany(initData.data);
-//   console.log("data was initialized");
-// };
-// initDB();  // only write the function not to write save to db
-app.get("/testing", async (req,res)=>{
-   let sampleListing= new Listing({
-      title :"My first ride",
-      description: "This is my last ride",
-      price: 10000,
-      location: "Pattaya", 
-      image: "https://www.google.com",
-      country :"Thailand"
+  app.engine("ejs",ejsmate)
+  app.set("view engine","ejs") 
+  app.set("views",path.join(__dirname,"views"))  
+  app.use(express.urlencoded({ extended: true }));
+  app.use(methodOverride("_method"));
+  app.use(express.static (path.join(__dirname,"/public")))
+  main()
+    .then(() => {
+      console.log("connected to DB");
+    })
+    .catch((err) => {
+      console.log(err) ;
     });
-   await sampleListing.save()
-   console.log("successful saved ")
-   res.send("success ")
-})
-app.get("/",(req,res)=>{
-   
-  res.send("this is the first page")
-}) 
-// Index route 
-app.get("/listing",async (req,res)=>{
-    const allListing= await Listing.find({})
-     res.render("./listings/index.ejs", { allListing }); 
-     }) 
-// add a new Post using Button in Index.ejs
-app.get("/listing/new", (req,res)=>{
-       res.render("./listings/new.ejs")
+  let port=3000;
+  app.listen(port, (req,res) => {
+    console.log("port is listing") 
   })
-  //create a route
-  app.post("/listings", WrapAsync(async(req,res,next)=>{
-     
-      const newlisting= new Listing(req.body.listings);
-       await newlisting.save()
-      res.redirect("/listing")
+
+  async function main() {
+    await mongoose.connect(MONGO_URL);
+  }
     
-  }));
-  // show a post
-app.get("/listings/:id", async(req,res)=>{
-       let {id} = req.params;
-       let listing = await Listing.findById(id);
-        res.render("./listings/show.ejs",{listing})
-})
-//Edit the post
-app.get("/listings/:id/edit", async(req,res)=>{
-     let {id} = req.params;
-      let listing = await Listing.findById(id);
-      res.render("./listings/edit.ejs",{listing})
-})
- //update 
- app.put("/listings/:id", async (req, res) => {
+  // const initDB = async () => {
+  //   await Listing.deleteMany({});
+  //   await Listing.insertMany(initData.data);
+  //   console.log("data was initialized");
+  // };
+  // initDB();  // only write the function not to write save to db
+  app.get("/testing", async (req,res)=>{
+    let sampleListing= new Listing({
+        title :"My first ride",
+        description: "This is my last ride",
+        price: 10000,
+        location: "Pattaya", 
+        image: "https://www.google.com",
+        country :"Thailand"
+      });
+    await sampleListing.save()
+    console.log("successful saved ")
+    res.send("success ")
+  })
+  app.get("/",(req,res)=>{
+    
+    res.send("this is the first page")
+  }) 
+  // Index route 
+  app.get("/listing",async (req,res)=>{
+      const allListing= await Listing.find({})
+      res.render("./listings/index.ejs", { allListing }); 
+      }) 
+  // add a new Post using Button in Index.ejs
+  app.get("/listing/new", (req,res)=>{
+        res.render("./listings/new.ejs")
+    })
+    //create a route
+    app.post("/listings", WrapAsync(async(req,res,next)=>{
+        listingSchema.validate(req.body)  
+        const newlisting= new Listing(req.body.listings);
+        await newlisting.save()
+        res.redirect("/listing")
+      
+    }));
+    // show a post 
+  app.get("/listings/:id", async(req,res)=>{
+        let {id} = req.params;
+        let listing = await Listing.findById(id);
+          res.render("./listings/show.ejs",{listing})
+  })
+  //Edit the post
+  app.get("/listings/:id/edit", async(req,res)=>{
+      let {id} = req.params;
+        let listing = await Listing.findById(id);
+        res.render("./listings/edit.ejs",{listing})
+  })
+  //update 
+  app.put("/listings/:id", async (req, res) => {
+      const { id } = req.params;
+      await Listing.findByIdAndUpdate(id, { ...req.body.listing });
+      res.redirect(`/listings/${id}`); // Redirect to the updated listing's show page
+  });
+  //delete the post 
+   app.delete("/listings/:id", async (req, res) =>{
     const { id } = req.params;
-    await Listing.findByIdAndUpdate(id, { ...req.body.listing });
-    res.redirect(`/listings/${id}`); // Redirect to the updated listing's show page
-});
-//delete the post 
-app.delete("/listings/:id", async (req, res) =>{
-   const { id } = req.params;
-    let deleted= await Listing.findByIdAndDelete(id);
-    res.redirect("/listing");
-} 
-) 
-//Handling the error raised by Post Method
-app.use((err, req, res, next) => {
-    //  let (statusCode=500,message="Something went wrong")=err
-     res.status(statusCode).send(message);
-})
-//route access to all 
-app.all("*",(req,res,next)=>{
-  next(new ExpressError(404,"Page Not Found"));
-})
+      let deleted= await Listing.findByIdAndDelete(id);
+      res.redirect("/listing");
+  } 
+  ) 
+  //Handling the error raised by Post Method
+  app.use((err, req, res, next) => {
+      let (statusCode=500,message="Something went wrong")=err
+      // res.status(statusCode).se nd(message);
+      res.render("error.ejs",{message})
+  })
+  //route access to all 
+  // app.all("*",(req,res,next)=>{
+  //   next(new ExpressError(404,"Page Not Found"));
+  // })
+   
+  
--- utils/ExpressError.js
+++ utils/ExpressError.js
@@ -1,4 +1,4 @@
-class ExpressError extends error{
+class ExpressError extends Error{
     constructor(statusCode, message ){
        super();
        this.statusCode = statusCode;
